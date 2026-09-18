d9d8af5e03cf2edae58a8ba30f78413e478cb182
Tiantian <tiantian5321.k@outlook.com>
2026-09-18 00:56:54 -0700

    add preferred_providers as CPUExecutionProvider to fix ONNX Runtime CoreML embedding error on MacBook

--- store.py
+++ store.py
@@ -63,7 +63,7 @@ class _OnnxEmbedder:
     def __init__(self):
         from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
 
-        self._ef = ONNXMiniLM_L6_V2()
+        self._ef = ONNXMiniLM_L6_V2(preferred_providers=["CPUExecutionProvider"])
 
     def encode(self, texts, show_progress_bar: bool = False):
         return [vector.tolist() for vector in self._ef(list(texts))]
